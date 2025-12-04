8c5d1c09f2aa028c2bf62179bb37c2c71f8556b9
Xian Dev <xiandev25@gmail.com>
2025-12-05 00:48:22 +0100

    Navigate to another app

--- app/src/main/java/com/example/cupcake/CupcakeScreen.kt
+++ app/src/main/java/com/example/cupcake/CupcakeScreen.kt
@@ -15,6 +15,8 @@
  */
 package com.example.cupcake
 
+import android.content.Context
+import android.content.Intent
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -137,9 +139,12 @@ fun CupcakeApp(
                 )
             }
             composable(route = CupcakeScreen.Summary.name) {
+                val context = LocalContext.current
                 OrderSummaryScreen(
                     orderUiState = uiState,
-                    onSendButtonClicked = { subject: String, summary: String -> },
+                    onSendButtonClicked = { subject: String, summary: String ->
+                        shareOrder(context = context, subject = subject, summary = summary)
+                    },
                     onCancelButtonClicked = { cancelOrderAndNavigateToStart(viewModel, navController)},
                     modifier = Modifier
                         .fillMaxHeight()
@@ -147,6 +152,20 @@ fun CupcakeApp(
             }
         }
     }
+}
+
+private fun shareOrder(context: Context, subject: String, summary: String) {
+    val intent = Intent(Intent.ACTION_SEND).apply {
+        type = "text/plain"
+        putExtra(Intent.EXTRA_SUBJECT, subject)
+        putExtra(Intent.EXTRA_TEXT, summary)
+    }
+    context.startActivity(
+        Intent.createChooser(
+            intent,
+            context.getString(R.string.new_cupcake_order)
+        )
+    )
 }
 
 private fun cancelOrderAndNavigateToStart(
